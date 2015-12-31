b9983e90f45423204b596842192f8f6ef2647bfe
Joël Gähwiler <joel.gaehwiler@gmail.com>
2015-12-31 13:16:22 +0100

    added doc

--- training.go
+++ training.go
@@ -1,5 +1,6 @@
 package gosom
 
+// A Training holds settings for a SOM training.
 type Training struct {
 	SOM                 *SOM
 	Steps               int
@@ -9,6 +10,7 @@ type Training struct {
 	FinalRadius         float64
 }
 
+// NewTraining returns a new Training.
 func NewTraining(som *SOM, steps int, ilr, flr, ir, fr float64) *Training {
 	return &Training{
 		SOM:                 som,
@@ -20,15 +22,18 @@ func NewTraining(som *SOM, steps int, ilr, flr, ir, fr float64) *Training {
 	}
 }
 
+// Progress returns the current progress based on step and steps.
 func (t *Training) Progress(step int) float64 {
 	return float64(step) / float64(t.Steps)
 }
 
+// LearningRate calculates the current learning rate.
 func (t *Training) LearningRate(step int) float64 {
 	r := t.InitialLearningRate - t.FinalLearningRate
 	return r * t.SOM.CF(t.Progress(step)) + t.FinalLearningRate
 }
 
+// Radius calculates the current radius.
 func (t *Training) Radius(step int) float64 {
 	r := t.InitialRadius - t.FinalRadius
 	return r * t.SOM.CF(t.Progress(step)) + t.FinalRadius
